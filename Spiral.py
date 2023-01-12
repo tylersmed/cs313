804daa9c363ca4af6f249ee0a89247a794cb47fd
Tyler Smedley <tylersmedley17@gmail.com>
2023-01-12 15:22:03 -0600

    yep i changed stuff

--- Spiral.py
+++ Spiral.py
@@ -13,16 +13,37 @@
 # Output: returns a 2-D list representing a spiral
 #         if n is even add one to n
 
+import math
 
-def createBlankSpiral(size):
-    spiral = [[x for x in size] for i in size]
-    print(spiral)
+def createBlankSpiral(spiralSize):
+    """creates a two dimensional list where the length of every list is the arbitrary integer 
+    sprialSize, contains only zeros, and then returns the created list"""
+    size = [0] * spiralSize
+    blankSpiral = [[x for x in size] for i in size]
+    return blankSpiral
+
+def addToSpiral(direction, travelLimit, rowPosition, columnPosition, number):
+    pass
 
 
-def create_spiral(n):
-    inFile = open("spiral.in", 'r')
-    spiral_Size = inFile.readline()
-    createBlankSpiral(spiral_Size)
+def create_spiral(spiralSize):
+
+    spiral = createBlankSpiral(spiralSize)
+    # calls the function to create a blank spiral
+
+    number = 1
+    # the number that will be added to the spiral
+    travelLimit = 1
+    # length the sprial can go in one direction before changing direction
+    direction = 'right'
+    # current direction that the spiral is added in
+    rowPosition = math.ceil(spiralSize/2)
+    columnPosition = math.ceil(spiralSize/2)
+    """Variables for the position on the spiral in terms of row and column.
+    math.ceil(spiralSize) makes the variables start in the middle of the sprial"""
+
+    return spiral
+    
     
 # Input: spiral is a 2-D list and n is an integer
 # Output: returns an integer that is the sum of the
@@ -33,11 +54,17 @@ def sum_adjacent_numbers(spiral, n):
 
 
 def main():
-    print("REMOVE THIS PRINT AND ADD YOUR CODE")
 
-# read the input file
+    inFile = open('spiral.in', 'r')
+    spiralSize = int(inFile.readline())
+    if spiralSize % 2 == 0: spiralSize += 1
+    """Takes the first line from spiral.in and converts it to an integer.
+    If the first number in the file was even, it is increased by one.
+    This will be the length and width of the spiral"""
 
-# create the spiral
+    # create the spiral
+    spiral = create_spiral(spiralSize)
+    print(spiral)
 
 # add the adjacent numbers
 
