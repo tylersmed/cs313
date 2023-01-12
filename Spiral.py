c8d75b92a9efd2f5458f82cfcedede9b24f74b0a
Tyler Smedley <tylersmedley17@gmail.com>
2023-01-12 14:00:50 -0600

    Template for Spiral.py and its infile

---
+++ Spiral.py
@@ -0,0 +1,41 @@
+#  File: Spiral.py
+#  Description:
+#  Student Name: Tyler Smedley
+#  Student UT EID: tws933
+#  Partner Name:
+#  Partner UT EID:
+#  Course Name: CS 313E
+#  Unique Number: 52020
+#  Date Created: 01/12/2023
+#  Date Last Modified:
+
+# Input: n is an odd integer between 1 and 100
+# Output: returns a 2-D list representing a spiral
+#         if n is even add one to n
+
+
+
+def create_spiral(n):
+    print("REMOVE THIS PRINT AND ADD YOUR CODE")
+    
+# Input: spiral is a 2-D list and n is an integer
+# Output: returns an integer that is the sum of the
+#         numbers adjacent to n in the spiral
+#         if n is outside the range return 0
+def sum_adjacent_numbers(spiral, n):
+    print("REMOVE THIS PRINT AND ADD YOUR CODE")
+
+
+def main():
+    print("REMOVE THIS PRINT AND ADD YOUR CODE")
+
+# read the input file
+
+# create the spiral
+
+# add the adjacent numbers
+
+# print the result
+
+if __name__ == "__main__":
+    main()
