e3501dd32927701241148b36e81fe7736ed791fa
Tyler Smedley <tylersmedley17@gmail.com>
2023-01-30 10:18:19 -0600

    Completed cipher assignment

--- Cipher.py
+++ Cipher.py
@@ -9,6 +9,8 @@ def crt_padded_enc(strng):
     chr_lst = list(strng)
 
     for row in range(sqr_len):
+        # replaces '*'s with chars from the string in a right to left,
+        # top to bottom direction
         for col in range(sqr_len):
             if not chr_lst:
                 break
@@ -20,24 +22,36 @@ def crt_padded_enc(strng):
 def crt_padded_dec(strng):
     # Creates a version of the string to be dectypted as a padded message
     sqr_len = math.ceil(math.sqrt(len(strng)))
-    padded_msg = [['*' for x in range(sqr_len)] for i in range(sqr_len)]
+    padded_msg = [[0 for x in range(sqr_len)] for i in range(sqr_len)]
+    numStars = (sqr_len ** 2) - len(strng)
     chr_lst = list(strng)
-    x = math.ceil(len(chr_lst) / sqr_len)
 
-    for col in range(sqr_len):
-        for i in range(x):
-            if not chr_lst:
-                break
+    num = 0
+    row = -1
+    while num < numStars:
+        # adds the needed number of '*'s to the matrix in a right to left, bottom to top direction
+        padded_msg[row].remove(0)
+        padded_msg[row].insert(0, "*")
+        num += 1
+        row -= 1
+        if row < -sqr_len:
+            row = -1
+    
+    for row in range(sqr_len):
+        # Adds the charachters in the string to the matrix in a right to left,
+        # top to bottom direction while skipping any index that contains '*'.
+        for col in range(sqr_len):
+            if padded_msg[row][col] == '*':
+                continue
             else:
-                padded_msg[col].remove('*')
-                padded_msg[col] += [chr_lst.pop(0)]
+                padded_msg[row][col] = chr_lst.pop(0)    
 
     return padded_msg
 
 def rotate_right(msg):
-    
+    # Takes a padded message and returns it rotated 90 degrees to the right
     lst = []
-    if len(msg[1]) == 1:
+    if len(msg[0]) == 1:
         lst = [msg[row][0] for row in range(len(msg)-1, -1, -1)]
         return lst
 
@@ -48,9 +62,9 @@ def rotate_right(msg):
         return lst + rotate_right(msg)
 
 def rotate_left(msg):
-
+    # takes a padded message and returns it rotated 90 degrees to the left
     lst = []
-    if len(msg[1]) == 1:
+    if len(msg[0]) == 1:
         lst = [msg[row][0] for row in range(len(msg))]
         return lst
 
@@ -82,8 +96,8 @@ def encrypt ( strng ):
 # Output: function returns an encrypted string 
 
 def decrypt ( strng ):
-    paddded_msg = crt_padded_dec(strng)
-    rotated_msg = rotate_left(paddded_msg)
+    padded_msg = crt_padded_dec(strng)
+    rotated_msg = rotate_left(padded_msg)
 
     decrypted_msg = ''
     for char in rotated_msg:
@@ -97,17 +111,18 @@ def decrypt ( strng ):
 def main():
     # read the strings P and Q from standard input
     P, Q = sys.stdin.read().strip().split("\n")
-    print(P, Q)
 
-    # encrypt the string P
-    print(encrypt(P), end = ' ')
+    # encrypt the string 
+    encP = encrypt(P)
 
     # decrypt the string Q
-    print(decrypt(Q))
+    decQ = decrypt(Q)
 
     # print the encrypted string of P
-    # and the decrypted string of Q
+    print(encP)
 
+    # and the decrypted string of Q
+    print(decQ)
      
 if __name__ == "__main__":
     main()
