40e7c9d7d542cf7a3222a0b6c80550a135927196
Tyler Smedley <tylersmedley17@gmail.com>
2023-02-02 22:45:23 -0600

    added documentation

--- employee.py
+++ employee.py
@@ -12,7 +12,7 @@
 import sys
 
 class Employee:
-
+    # All employees have atributes of name, id, and salary
     def __init__(self, **kwargs):
         self.name = kwargs.get('name')
         self.id = kwargs.get('id')
@@ -20,7 +20,7 @@ class Employee:
         self.title = "Employee"
 
     def __str__(self):
-    # Add your code here!
+    # returns the employee's job title, name, id, and salary
         return self.title + "\n{},{},{}".format(self.name, self.id, self.salary)
 
 ############################################################
@@ -28,13 +28,14 @@ class Employee:
 ############################################################
 
 class Permanent_Employee(Employee):
-
+    # Permanent employees also have an atribute of benefits
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.benefits = kwargs.get('benefits')
         self.title = "Permanent_Employee"
 
     def cal_salary(self):
+        # having benefits modifies salary ammount
         if 'retirement' in self.benefits and 'health_insurance' in self.benefits:
             return self.salary * 0.7
         elif 'health_insurance' in self.benefits:
@@ -45,6 +46,7 @@ class Permanent_Employee(Employee):
             return round(float(self.salary), 1)
 
     def __str__(self):
+        # Adds the value for benefits to the general employee string
         return format(super().__str__() + ',' + str(self.benefits))
 
 ############################################################
@@ -52,17 +54,19 @@ class Permanent_Employee(Employee):
 ############################################################
 
 class Manager(Employee):
-
+    # Managers also get a bonus
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.bonus = kwargs.get('bonus')
         self.title = "Manager"
 
     def cal_salary(self):
+        # bonus is added to base pay to get salary
         salary  = float(self.salary + self.bonus)
         return round(salary, 1)
 
     def __str__(self):
+        # adds the value of bonus to the general employee string
         return super().__str__() + ',' + str(self.bonus)
 
 
@@ -71,17 +75,19 @@ class Manager(Employee):
 ############################################################
 
 class Temporary_Employee(Employee):
-
+    # temporary employees have an additional atribute of hours worked
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.hours = kwargs.get('hours')
         self.title = "Temporary_Employee"
 
     def cal_salary(self):
+        # temp employee salary is num of hours worked * hourly pay
         salary = float(self.salary * self.hours)
         return round(salary, 1)
 
     def __str__(self):
+        # adds hours worked to the general employee string
         return super().__str__() + ',' + str(self.hours)
 
 
@@ -91,18 +97,20 @@ class Temporary_Employee(Employee):
 
 
 class Consultant(Temporary_Employee):
-
+    # consultants are a subclass of temporary employee with an additional attribute of travel
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.travel = kwargs.get('travel')
         self.title = "Consultant"
 
     def cal_salary(self):
+        # consultant salary is calculated like a temp employee's plus num of times traveled * 1000
         travel_pay = self.travel * 1000
         salary = float(super().cal_salary() + travel_pay)
         return round(salary, 1)
 
     def __str__(self):
+        # adds value for travel to the string for temporary employees
         return format(super().__str__() + ',' + str(self.travel))
 
 
@@ -112,17 +120,20 @@ class Consultant(Temporary_Employee):
 
 
 class Consultant_Manager(Manager, Consultant):
-
+    # consultant managers have attribues of both managers and consultants
     def __init__(self,  **kwargs):
         Manager.__init__(self, **kwargs)
         Consultant.__init__(self, **kwargs)
         self.title = "Consultant_Manager"
 
     def cal_salary(self):
+        # salary is calculated the same as a consultants plus the manager bonus
         salary = float(Consultant.cal_salary(self) + self.bonus)
         return round(salary, 1)
 
     def __str__(self):
+        # prints the values for the consultant attributes on one line
+        # and the values for the manager attribues on the next line
         return Consultant.__str__(self) + ',' + self.title + '\n' + self.name +',' + self.id + ',' + str(self.salary) + ',' + str(self.bonus)
 
 
