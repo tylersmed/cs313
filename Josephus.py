68d94eb63e1cf72dd518c58a1ef15dad8d196b2e
Tyler Smedley <tylersmedley17@gmail.com>
2023-03-02 19:09:46 -0600

    Completed constructor, insert, find, and a helper func for circularList class

--- Josephus.py
+++ Josephus.py
@@ -1,50 +1,96 @@
 import sys
 
 class Link(object):
+	# Constructor
+	def __init__(self, data, next = None):
+		self.data = data
+		self.next = next
 
+	def __str__(self):
+		return str(self.data)
 
 class CircularList(object):
-  # Constructor
-  def __init__ ( self ):
+	# Constructor
+	def __init__ ( self ):
+		self.first = None
 
-  # Insert an element (value) in the list
-  def insert ( self, data ):
+    # Insert an element (value) in the list
+	def insert ( self, data ):
+		newLink = Link(data)
+		if self.first == None:
+			self.first = newLink
+			self.first.next = self.first
+		else:
+			newLink.next = self.first.next
+			self.first.next = newLink
+			self.first = newLink
 
-  # Find the Link with the given data (value)
-  # or return None if the data is not there
-  def find ( self, data ):
+    # Find the Link with the given data (value)
+    # or return None if the data is not there
+	def find ( self, data ):
+		if self.data_in_lst(data):
+			current = self.first.next
+			while current.data != data:
+				current = current.next
+			return current
+		else:
+			return None
 
-  # Delete a Link with a given data (value) and return the Link
-  # or return None if the data is not there
-  def delete ( self, data ):
+	def data_in_lst(self, data):
+		start = self.first.data
+		current = self.first
+		while current.next.data != start:
+			if current.data == data:
+				return True
+			current = current.next
+		if current.data == data:
+			return True
+		return False
+	
+    # Delete a Link with a given data (value) and return the Link
+    # or return None if the data is not there
+	def delete ( self, data ):
+		pass
 
-  # Delete the nth Link starting from the Link start
-  # Return the data of the deleted Link AND return the
-  # next Link after the deleted Link in that order
-  def delete_after ( self, start, n ):
+    # Delete the nth Link starting from the Link start
+    # Return the data of the deleted Link AND return the
+    # next Link after the deleted Link in that order
+	def delete_after ( self, start, n ):
+		pass
 
-  # Return a string representation of a Circular List
-  # The format of the string will be the same as the __str__
-  # format for normal Python lists
-  def __str__ ( self ):
+    # Return a string representation of a Circular List
+    # The format of the string will be the same as the __str__
+    # format for normal Python lists
+	def __str__ ( self ):
+		first = self.first.next.data
+		lst = ''
+		current = self.first.next
+		while current.next.data != first:
+			lst =  lst + str(current.data) + ', '
+			current = current.next
+		return '[' + lst + str(self.first.data) + ']'
 
 def main():
-  # read number of soldiers
-  line = sys.stdin.readline()
-  line = line.strip()
-  num_soldiers = int (line)
+    # read number of soldiers
+	line = sys.stdin.readline()
+	line = line.strip()
+	num_soldiers = int (line)
 
-  # read the starting number
-  line = sys.stdin.readline()
-  line = line.strip()
-  start_count = int (line)
+    # read the starting number
+	line = sys.stdin.readline()
+	line = line.strip()
+	start_count = int (line)
 
-  # read the elimination number
-  line = sys.stdin.readline()
-  line = line.strip()
-  elim_num = int (line)
+    # read the elimination number
+	line = sys.stdin.readline()
+	line = line.strip()
+	elim_num = int (line)
 
-  # your code
+    # create the circular linked list
+	soldierCircle = CircularList()
+	for i in range(1, num_soldiers+1):
+		soldierCircle.insert(i)
+	print(soldierCircle)
 
 if __name__ == "__main__":
-  main()
+    main()
